6037fee6b06fd6c51c553e83d257a74a2e1a20e2
gsloing <gabysloing@gmail.com>
2024-02-23 16:13:24 +0800

    memanggil component di App.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from 'react-native';
+import Registration from "./components/Registration";
+import UsersList from "./components/UsersList";
+
+const App = () => {
+    return <Registration />
+    //return <UsersList />
+};
+
+export default App;
